95b1ab0f9da1fc62287cdf8fb4c032e54d606236
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-21 16:26:08 -0300

    commit resolução até o exercicio 4

---
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco/4.1.js
@@ -0,0 +1,33 @@
+const a = 5;
+const b = 10;
+
+    const adicao = a + b;
+    const subtracao = a - b;
+    const multiplicacao = a * b;
+    const divisao = a / b;
+    const modulo = a % b;
+
+   //exercicio 2
+
+   const n1 = 2;
+   const n2= 3;
+   const n3 = 5;
+
+   if (n1 > n2) {
+    console.log(n1)
+   } else {
+    console.log(n2)
+   }
+
+   // exercicio 3
+
+   if (n1 > n2) {
+    console.log(n1)
+   } else if (n2>n2) {
+    console.log(n2)
+   } else {
+    console.log(n3)
+   }
+
+   // exercicios 4
+   
