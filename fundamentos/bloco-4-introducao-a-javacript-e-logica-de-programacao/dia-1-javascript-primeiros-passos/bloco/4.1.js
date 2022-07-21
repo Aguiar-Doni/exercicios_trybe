f7a0d698525debb6e5c58a9320ee59db2a6f7320
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-21 20:03:30 -0300

    resolução do exercicio 5

--- fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco/4.1.js
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco/4.1.js
@@ -1,3 +1,6 @@
+   //1.Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, 
+   //definidas no começo com os valores que serão operados. Faça programas para:
+
 const a = 5;
 const b = 10;
 
@@ -7,11 +10,11 @@ const b = 10;
     const divisao = a / b;
     const modulo = a % b;
 
-   //exercicio 2
+   //2.Faça um programa que retorne o maior de dois numeros.
 
-   const n1 = 2;
-   const n2= 3;
-   const n3 = 5;
+const n1 = 2;
+const n2= 3;
+const n3 = 5;
 
    if (n1 > n2) {
     console.log(n1)
@@ -19,7 +22,7 @@ const b = 10;
     console.log(n2)
    }
 
-   // exercicio 3
+   //3.Faça um programa que retorne o maior de três numeros.
 
    if (n1 > n2) {
     console.log(n1)
@@ -29,7 +32,8 @@ const b = 10;
     console.log(n3)
    }
 
-   // exercicios 4
+   //4.Faça um programa que, dado um valor definido numa constante, retorne 'positive' se esse valor for positivo;
+   //'negative' se esse valor fot negativo e 'zero' caso contrário.
 
    if (n1 > 0) {
     console.log ('positive')
@@ -39,4 +43,20 @@ const b = 10;
     console.log('zero')
    }
    
-   //exercicio 5
+   //5.Faça um programa que defina três constantes com valores de três ângulos internos de um triângulo.
+   //retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário.Se algum ângulo for inválido
+   // deve retornar uma menssagem de erro.
+
+   const ang1 = 80
+   const ang2 = 45
+   const ang3 = -5
+
+   if (ang1 + ang2 > ang3 && ang1 + ang3 > ang2 && ang2 + ang3 > ang1) {
+    console.log('true') 
+
+   } else if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+        console.log('algo deu errado') 
+        
+   } else if (ang1 + ang2 + ang3 != 180) {
+    console.log('false')
+   }  
