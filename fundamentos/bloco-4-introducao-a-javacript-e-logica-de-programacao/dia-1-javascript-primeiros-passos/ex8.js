b3d13b40ba29a4f0fedca3325a31579fdc08e6a1
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-22 16:27:45 -0300

    resolução do exercicio 8

--- fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex8.js
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex8.js
@@ -1,1 +1,11 @@
- //8.Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+//8.Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+
+const n1 = 8;
+const n2 = 1;
+const n3 =3;
+
+if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) {
+    console.log('true')    
+} else {
+    console.log('false');
+}
