93a40ad53e4606cd2eb2d9f4bcce008b1dacc29c
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-25 16:26:51 -0300

    resolução ate o exercicio 5 do dia 4.2

---
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-2-javacript-Arrays e loop-For/ex3.js
@@ -0,0 +1,9 @@
+//3.Calcule a médio aritimetica do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  media += numbers[index] / numbers.length;
+}
+
+console.log(media);
