48623f701f1eea2d429fc74afef507d8d8cd6517
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-25 20:53:52 -0300

    resolução de todos exercicios ate o 9 do dia 4.2

--- fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-2-javacript-Arrays e loop-For/ex3.js
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-2-javacript-Arrays e loop-For/ex3.js
@@ -1,4 +1,4 @@
-//3.Calcule a médio aritimetica do array
+//3.Calcule a média aritimetica do array
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
--- fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-2-javacript-Arrays e loop-For/ex5.js
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-2-javacript-Arrays e loop-For/ex5.js
@@ -1,6 +1,6 @@
 //5.Descubra qual maior valor contido no array e imprima-o;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 165];
 let maiorNumero = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
