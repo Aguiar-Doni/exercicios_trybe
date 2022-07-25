d64b31d505907680d444ef59be070a1db1656281
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-07-25 15:42:09 -0300

    correção do exercicio 11

--- fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex11.js
+++ fundamentos/bloco-4-introducao-a-javacript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex11.js
@@ -25,7 +25,7 @@
 //Resultado: R$ 2.612,55
 //Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
 
-const salarioBruto = 5876.98;
+const salarioBruto = 6000;
 let descontoInss;
 let salarioBase;
 let salarioIR;
@@ -33,38 +33,37 @@ let iR;
 
 //DEDUZINDO INSS
 
-if ((salarioBruto <= 1.556, 94)) {
-  salarioInss = (salarioBruto / 100) * 8;
-  salarioBase = salarioBruto - salarioInss;
-} else if (salarioBruto <= 2594.92 || salarioBruto > 1556.94) {
-  salarioInss = (salarioBruto / 100) * 9;
-  salarioBase = salarioBruto - salarioInss;
-} else if (salarioBruto <= 5189.82 || salarioBruto > 2594.92) {
-  salarioInss = (salarioBruto / 100) * 11;
-  salarioBase = salarioBruto - salarioInss;
+if (salarioBruto <= 1556.94) {
+  descontoInss = (salarioBruto / 100) * 8;
+  salarioBase = salarioBruto - descontoInss;
+} else if (salarioBruto <= 2594.92 && salarioBruto > 1556.94) {
+  descontoInss = (salarioBruto / 100) * 9;
+  salarioBase = salarioBruto - descontoInss;
+} else if (salarioBruto <= 5189.82 && salarioBruto > 2594.92) {
+  descontoInss = (salarioBruto / 100) * 11;
+  salarioBase = salarioBruto - descontoInss;
 } else if (salarioBruto > 5189.82) {
-  salarioInss = salarioBruto - 570.88;
-  salarioBase = salarioBruto - salarioInss;
+  salarioBase = salarioBruto - 570.88;
 }
 
 // DEDUZINDO IMPOSTO DE RENDA
 
 if (salarioBase < 1903.98) {
   console.log("insento de imposto de renda");
-} else if (salarioBase > 1903.98 || salarioBase <= 2826.65) {
-  iR = (salarioBase / 100) * 7.5;
+} else if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
+  iR = (salarioBase / 100) * 7.5 - 142.8;
   salarioIR = salarioBase - iR;
   console.log("seu salario deduzido do imposto de renda é R$:", salarioIR);
-} else if (salarioBase > 2826.65 || salarioBase <= 3751.05) {
-  iR = (salarioBase / 100) * 15;
+} else if (salarioBase > 2826.65 && salarioBase <= 3751.05) {
+  iR = (salarioBase / 100) * 15 - 354.8;
   salarioIR = salarioBase - iR;
   console.log("seu salario deduzido do imposto de renda é R$:", salarioIR);
-} else if (salarioBase > 3751.05 || salarioBase <= 4664.68) {
-  iR = (salarioBase / 100) * 22.5;
+} else if (salarioBase > 3751.05 && salarioBase <= 4664.68) {
+  iR = (salarioBase / 100) * 22.5 - 636.13;
   salarioIR = salarioBase - iR;
   console.log("seu salario deduzido do imposto de renda é R$:", salarioIR);
 } else if (salarioBase > 4664.68) {
-  iR = (salarioBase / 100) * 27.5;
+  iR = (salarioBase / 100) * 27.5 - 869.36;
   salarioIR = salarioBase - iR;
   console.log("seu salario deduzido do imposto de renda é R$:", salarioIR);
 }
