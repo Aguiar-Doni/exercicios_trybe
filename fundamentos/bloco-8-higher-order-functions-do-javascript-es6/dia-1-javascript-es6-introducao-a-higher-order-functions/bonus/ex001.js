0ee65eb416b5b26422f5e86f09ce1aa4f9a6dae4
Rafael Aguiar <rafa.donizete2014@gmail.com>
2022-08-19 22:07:50 -0300

    arrumando

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus/ex001.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus/ex001.js
@@ -34,13 +34,13 @@ const mage = {
 /* 2 - Crie uma função que retorna o dano causado pelo warrior.
 O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo). */
 
-const damageWrrior = (warrior) => {
+const damageWarrior = (warrior) => {
     const minDmg = warrior.strength
     const maxDmg = Math.floor(Math.random() * (minDmg * warrior.weaponDmg));
     const warriorDmg = maxDmg > minDmg ? maxDmg : minDmg;
     return warriorDmg;
 };
-
+console.log(damageWarrior(warrior));
 /* 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano 
