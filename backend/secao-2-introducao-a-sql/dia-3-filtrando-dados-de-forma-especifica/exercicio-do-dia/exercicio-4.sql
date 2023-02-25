2178fe3926d9549ee69ad49eee3a185e3bbb1380
Aguiar-Doni <rafa.donizete2014@gmail.com>
2023-02-24 21:03:46 -0300

    exercicios/backend-2.3

---
+++ backend/secao-2-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercicio-do-dia/exercicio-4.sql
@@ -0,0 +1,5 @@
+-- 4. Avante, falta pouco! Escreva uma query para exibir todas as informações de supplier que são empresas limitadas (LTDA).
+-- Ordene esses resultados em ordem alfabética decrescente.
+USE store;
+SELECT * FROM suppliers
+WHERE name LIKE '%LTDA%' ORDER BY name DESC;
